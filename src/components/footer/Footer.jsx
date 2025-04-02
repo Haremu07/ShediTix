bb8dd0c7a81ef9984e79f5c7e2acbbb2da07307c
Etopia1 <jolaetopia81@gmail.com>
2025-04-02 22:55:44 +0100

    ll

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -105,7 +105,7 @@ const Footer = () => {
             <a href="#">
               <FaTwitter />
             </a>
-            <a href="#">
+            <a>
               <FaLinkedin />
             </a>
             <a href="#">
