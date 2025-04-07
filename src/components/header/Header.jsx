25c3bf3115c94469cf8c54e06860464c5af6ed78
Etopia1 <jolaetopia81@gmail.com>
2025-04-07 14:51:17 +0100

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="header">
       <div className="header-container">
         <a href="/" className="logo-link">
-          <span className="logo-text">ScheduTix</span>
+          <span className="logo-text">SchediTix</span>
         </a>
 
         <nav className="nav">
