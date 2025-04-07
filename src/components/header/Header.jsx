e6fd867b3944faee052d47641fadc475f34ace72
Etopia1 <jolaetopia81@gmail.com>
2025-04-07 10:58:28 +0100

    hhh

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,39 @@
-import "./Header.css"
+import React from 'react'
+import "../header/Header.css"
+import { FiHeart, FiSearch } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <div className="headerBody">header</div>
+    <div>
+        <header>
+      <div>
+        <p>
+          <img src="../" alt="" />
+        </p>
+
+        <nav>
+          <p>Event Categories</p>
+        </nav>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Search events (Tech,Comedy,Concerts)"
+          />
+          <FiSearch />
+        </div>
+
+        <div>
+          <button>
+            <FiHeart />
+          </button>
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </header>
+
+    </div>
   )
 }
 
